bc111f2dad87d3851158821a9312389bc179919d
James Whitmarsh <jwhitmarsh@dresources.com>
2015-09-27 15:01:53 +0100

    added custom find method

--- common/models/player.js
+++ common/models/player.js
@@ -1,3 +1,11 @@
+var app = require('../../server/server');
+
 module.exports = function(Player) {
+
+    Player.on('attached', function(obj) {
+        Player.find = function(filter, cb) {
+            app.models.Detail.latestAll(cb);
+        }
+    });
 
 };
